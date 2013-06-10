8de5eebe5f3df5cda70d38a7deea7d4220c64e57
Marcel Mathias Nolte <marcel.nolte@noltecomputer.de>
2013-06-10 13:32:49 +0200

    first

---
+++ CSV-Kombinierer/frmMain.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CSV_Kombinierer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblHowTo = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.chkRemoveFirstLine = new System.Windows.Forms.CheckBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHowTo
+            // 
+            this.lblHowTo.AutoSize = true;
+            this.lblHowTo.Location = new System.Drawing.Point(12, 9);
+            this.lblHowTo.Name = "lblHowTo";
+            this.lblHowTo.Size = new System.Drawing.Size(231, 13);
+            this.lblHowTo.TabIndex = 0;
+            this.lblHowTo.Text = "Ziehen Sie Dateien und Ordner in die Dateiliste:";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.AllowDrop = true;
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(15, 38);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(499, 173);
+            this.lstFiles.TabIndex = 1;
+            // 
+            // chkRemoveFirstLine
+            // 
+            this.chkRemoveFirstLine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkRemoveFirstLine.AutoSize = true;
+            this.chkRemoveFirstLine.Location = new System.Drawing.Point(15, 249);
+            this.chkRemoveFirstLine.Name = "chkRemoveFirstLine";
+            this.chkRemoveFirstLine.Size = new System.Drawing.Size(117, 17);
+            this.chkRemoveFirstLine.TabIndex = 2;
+            this.chkRemoveFirstLine.Text = "Kopfzeile entfernen";
+            this.chkRemoveFirstLine.UseVisualStyleBackColor = true;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point(152, 217);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(362, 49);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "Kombinieren und Speichern";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Enabled = false;
+            this.btnClear.Location = new System.Drawing.Point(15, 220);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(131, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Liste leeren";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 278);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.chkRemoveFirstLine);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.lblHowTo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "CSV-Kombinierer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHowTo;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.CheckBox chkRemoveFirstLine;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ CSV-Kombinierer/frmMain.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSV_Kombinierer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            lstFiles.DragEnter += (o, e) =>
+                {
+                    if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                    {
+                        string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                        foreach (string file in files)
+                        {
+                            if (!System.IO.Directory.Exists(file) && !file.ToLower().EndsWith(".csv"))
+                            {
+                                return;
+                            }
+                        }
+                        e.Effect = DragDropEffects.All;
+                    }
+                };
+            lstFiles.DragDrop += (o, e) =>
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    foreach (string file in files)
+                    {
+                        if (System.IO.Directory.Exists(file))
+                        {
+                            Queue<string> ToDo = new Queue<string>(new string[] { file });
+                            while (ToDo.Count > 0)
+                            {
+                                string current = ToDo.Dequeue();
+                                foreach (string item in System.IO.Directory.GetFiles(current))
+                                {
+                                    if (item.ToLower().EndsWith(".csv"))
+                                    {
+                                        lstFiles.Items.Add(item);
+                                    }
+                                }
+                                foreach (string directory in System.IO.Directory.GetDirectories(current))
+                                {
+                                    ToDo.Enqueue(directory);
+                                }
+                            }
+                        }
+                        else if (file.ToLower().EndsWith(".csv"))
+                        {
+                            lstFiles.Items.Add(file);
+                        }
+                    }
+                    btnClear.Enabled = btnExport.Enabled = lstFiles.Items.Count > 0;
+                };
+            btnClear.Click += (o,e) => 
+                {
+                    lstFiles.Items.Clear();
+                    btnClear.Enabled = btnExport.Enabled = false;
+                };
+            btnExport.Click += (o, e) =>
+                {
+                    using (SaveFileDialog sfd = new SaveFileDialog())
+                    {
+                        if (!String.IsNullOrWhiteSpace(Properties.Settings.Default.LastSave))
+                        {
+                            sfd.FileName = Properties.Settings.Default.LastSave;
+                        }
+                        sfd.OverwritePrompt = true;
+                        if (sfd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+                        {
+                            int lineCount = 0;
+                            using (System.IO.StreamWriter output = new System.IO.StreamWriter(sfd.FileName, false))
+                            {
+                                bool isFirstFile = true;
+                                foreach (string file in lstFiles.Items)
+                                {
+                                    bool isFirstLine = true;
+                                    foreach (string line in System.IO.File.ReadAllLines(file))
+                                    {
+                                        if (!isFirstLine || isFirstFile || !chkRemoveFirstLine.Checked)
+                                        {
+                                            output.WriteLine(line);
+                                            lineCount++;
+                                        }
+                                        isFirstLine = false;
+                                    }
+                                    isFirstFile = false;
+                                }
+                                output.Flush();
+                                output.Close();
+                            }
+                            MessageBox.Show(this, lineCount.ToString() + " Zeilen aus " + lstFiles.Items.Count + " Dateien wurden in die Datei\n'" + sfd.FileName + "' geschrieben.", "Fertig!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Properties.Settings.Default.LastSave = sfd.FileName;
+                            Properties.Settings.Default.Save();
+                        }
+                    }
+                };
+        }
+    }
+}
